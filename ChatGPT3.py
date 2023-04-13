3f48ae5abc0df15b20799886475cc59470489b79
bbbqmqgovnocoder <130691267+bbbqmqgovnocoder@users.noreply.github.com>
2023-04-13 20:14:07 +0300

    Add files via upload

---
+++ ChatGPT3.py
@@ -0,0 +1,110 @@
+#           https://t.me/bbbqmq_govnocoder
+#
+# meta developer: @bbbqmq_govnocoder
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from .. import loader, utils
+
+def register(cb):
+    cb(ChatGPT3())
+
+
+class ChatGPT3(loader.Module):
+    """GPT-3 Без использования OpenAI токена"""
+
+    strings = {"name": "ChatGPT3"}
+
+    def __init__(self):
+        self.name = self.strings["name"]
+        self._me = None
+        self._ratelimit = []
+
+    async def client_ready(self, client, db):
+        self._db = db
+        self._client = client
+        self.me = await client.get_me()
+
+    async def chatcmd(self, event):
+        """Вопрос"""
+        user_msg = """/chat {}""".format(utils.get_args_raw(event))
+        global reply_and_text
+        reply_and_text = False
+        if event.fwd_from:
+            return
+        if not event.reply_to_msg_id:
+            self_mess = True
+            if not user_msg:
+                await event.edit(".chat")
+                return
+        elif event.reply_to_msg_id and user_msg:
+            reply_message = await event.get_reply_message()
+            reply_and_text = True
+            self_mess = True
+        elif event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            self_mess = False
+        chat = "@gpttgchatbot"
+        await event.edit("<code>GPT-3 генерирует ответ, ожидайте.</code>")
+        async with event.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=6025351724)
+                )
+                if not self_mess:
+                    await event.client.forward_messages(chat, reply_message)
+                else:
+                    await event.client.send_message(chat, user_msg)
+                response = await response
+            except YouBlockedUserError:
+                await event.reply("<code>Разблокируй </code>@GPTtgChatBot")
+                return
+            await event.delete()
+            if reply_and_text:
+                await event.client.send_message(
+                    event.chat_id, response.message, reply_to=reply_message.id
+                )
+            else:
+                await event.client.send_message(event.chat_id, response.message)
+
+    async def imgcmd(self, event):
+        """Описание картинки"""
+        user_msg = """/img {}""".format(utils.get_args_raw(event))
+        global reply_and_text
+        reply_and_text = False
+        if event.fwd_from:
+            return
+        if not event.reply_to_msg_id:
+            self_mess = True
+            if not user_msg:
+                await event.edit(".img")
+                return
+        elif event.reply_to_msg_id and user_msg:
+            reply_message = await event.get_reply_message()
+            reply_and_text = True
+            self_mess = True
+        elif event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+            self_mess = False
+        chat = "@gpttgchatbot"
+        await event.edit("<code>GPT-3 генерирует картинку, ожидайте.</code>")
+        async with event.client.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=6025351724)
+                )
+                if not self_mess:
+                    await event.client.forward_messages(chat, reply_message)
+                else:
+                    await event.client.send_message(chat, user_msg)
+                response = await response
+            except YouBlockedUserError:
+                await event.reply("<code>Разблокируй </code>@GPTtgChatBot")
+                return
+            await event.delete()
+            if reply_and_text:
+                await event.client.send_message(
+                    event.chat_id, response.message, reply_to=reply_message.id
+                )
+            else:
+                await event.client.send_message(event.chat_id, response.message)
